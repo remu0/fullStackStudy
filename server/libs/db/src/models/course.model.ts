908432fb85c0209c50cf2cbce1d2d4b4f290bb56
remu0 <761258460@qq.com>
2020-03-07 19:03:24 +0800

    第二章完成

--- server/libs/db/src/models/course.model.ts
+++ server/libs/db/src/models/course.model.ts
@@ -4,16 +4,23 @@ import { Episode } from './episode.model'
 
 @modelOptions({
     schemaOptions: {
-        timestamps: true
+        timestamps: true,
+        toJSON: { virtuals: true }
     }
 })
 export class Course {
-    @ApiProperty({ description: '课程名称'})
+    @ApiProperty({ description: '课程名称' })
     @prop()
     name: string
 
-    @ApiProperty({ description: '封面图'})
+    @ApiProperty({ description: '封面图' })
     @prop()
     cover: string
-    
+
+    @arrayProp({
+        ref: 'Episode',
+        localField: '_id',
+        foreignField: 'course'
+    })
+    episodes: Ref<Episode>[]
 }
