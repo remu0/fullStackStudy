67a1e4fa79e08ef8667b15d1e8b547e3271df89e
remu0 <761258460@qq.com>
2020-03-07 09:49:05 +0800

    原因备注

---
+++ admin/src/views/Main.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <el-menu mode="vertical" style="height:100vh;" :default-active="$router.path" router>
+        <el-submenu
+          v-for="(item, index) in menu.items"
+          :index="`menu-item-${index}`"
+          :key="`menu-item-${index}`"
+        >
+          <template slot="title">{{item.tittle}}</template>
+          <el-menu-item
+            v-for="(subItem, subIndex) in item.items"
+            :key="`menu-item-${index}-${subIndex}`"
+            :index="subItem.path"
+          >{{subItem.title}}</el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <h3>全栈之巅 - 后台管理界面</h3>
+      </el-header>
+      <el-main>
+        <router-view :key="$route.path"></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Main extends Vue {
+  menu = {
+    items: [
+      {
+        tittle: "内容管理",
+        items: [
+          { title: "首页", path: "/" },
+          { title: "课程管理", path: "/courses/list" },
+          { title: "课时管理", path: "/episodes/list" }
+        ]
+      },
+      {
+        tittle: "运营管理",
+        items: [
+          { title: "用户管理", path: "/users/list" },
+        ]
+      }
+    ]
+  };
+}
+</script>
+
+<style>
+</style>
